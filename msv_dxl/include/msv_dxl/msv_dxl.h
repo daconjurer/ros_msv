feae335dcaa97611c494c64d6e2d790a52f9faee
Victor Sandoval-Luna <stb.4421@gmail.com>
2019-10-22 15:50:37 -0500

    msv_dxl package update

---
+++ msv_dxl/include/msv_dxl/msv_dxl.h
@@ -0,0 +1,91 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2019, Robótica de la Mixteca
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Universidad Tecnológica de la Mixteca nor
+ *     the names of its contributors may be used to endorse or promote
+ *     products derived from this software without specific prior
+ *     written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+ 
+/////////////////////////////////////////////////////////////////////////////////////////
+/// @file ROS header for the dxl class of the MSV-01 rescue robot.
+/// @author Victor Esteban Sandoval-Luna
+///
+/////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef MSV_DXL_H_
+#define MSV_DXL_H_
+
+#define COM_START               0x0A
+#define COM_STOP                0x0B
+#define OBC_START               0x10
+#define ARDI_READY              0x20
+
+#include <ros/ros.h>
+#include <msv_main/port_handler.h>
+#include <boost/asio.hpp>
+#include <sensor_msgs/Joy.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Float32MultiArray.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/UInt8MultiArray.h>
+#include <std_msgs/MultiArrayDimension.h>
+#include <string.h>
+
+class MsvDxl
+{
+  private:
+    // Serial port
+    msv::PortHandler dxl_port;
+
+    ros::NodeHandle n_dxl;
+    ros::Publisher pub_angles;
+    ros::Publisher pub_base;
+
+    // For the RGBD orientation system control
+    ros::Subscriber sub_mode;
+    ros::Subscriber sub_joy;
+    
+    // Messages to be published
+    std_msgs::Float32MultiArray ocmd_angles;
+    std_msgs::Float32 arm_base;
+
+    int on;
+    int x, y, up, down;
+    
+    int send;
+
+    void joyCallback (const sensor_msgs::Joy::ConstPtr& joy);
+    void modeCallback (const std_msgs::String::ConstPtr& mode);
+
+  public:
+    MsvDxl ();
+};// End of class MsvDxl
+
+#endif
